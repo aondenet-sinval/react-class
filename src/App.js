a3a85065a4c4b13881af0951122a022ac1dab443
aondenet-sinval <sinvalsatv@gmail.com>
2022-09-24 21:48:41 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class Search extends React.Component {
   render() {
     // console.log('props search', this.props.result);
     const result = this.props.result
-    //Verificando se é array para aplicar a função map
-    console.log('result.produtos in Search is array? ', Array.isArray(result.produtos));
+    //Verificando se é array para correta aplicação da função map
+    console.log('result.produtos in Search is array? ',
+                  Array.isArray(result.produtos));
     const produtos = result.produtos.map((produto, index)=>
     <li key={index}>
       Código do produto: {produto.codigo}.
